e37071622073ed8ffa9ddecdab327f7b7106d13b
Teemu Puntila <teemu@pop-os.localdomain>
2023-11-15 11:12:03 +0200

    remove unused utility and use generateStaticParams instead of staticPaths

--- src/app/portfolio/[title]/page.tsx
+++ src/app/portfolio/[title]/page.tsx
@@ -1,43 +1,29 @@
 import { getPortfolioData } from '@/utils/getPortfolioData/getPortfolioData'
 import styles from './page.module.css'
-import { GetStaticPaths, Metadata } from 'next'
-import { getAllPortfolioPaths } from '@/utils/getPortfolioData/getAllPortfolioPaths'
 import CodeTitle from '@/components/codeTitle/codeTitle'
 import PortfolioItems from '@/components/portfolioItem/portfolioItem'
+import pageData from '../../../../public/portfolioData.json'
+import { notFound } from 'next/navigation'
 
 type Props = {
     params: { title: string }
 }
-
-export const getStaticPaths: GetStaticPaths<{ title: string }> = async () => {
-    const paths = await getAllPortfolioPaths()
-    return {
-        paths,
-        fallback: false,
-    }
-}
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    return {
-        title: `Teemu Puntila - Portfolio - ${params.title}`,
-        description: `Teemu Puntila Portfolio ${params.title}`,
-        openGraph: {
-            title: `Teemu Puntila - Portfolio - ${params.title}`,
-            description: `Teemu Puntila Portfolio ${params.title}`,
-            images: [`/favicon.ico`],
-        },
-    }
+export async function generateStaticParams() {
+    return pageData.data.map((item) => ({ title: item.title, data: item }))
 }
 const Page = async ({ params }: Props) => {
     const data = await getPortfolioData(params.title)
-    return (
-        <div className={styles.container}>
-            <CodeTitle text={data.codeTitle} />
-            <PortfolioItems data={data} />
-            <div />
-            <div />
-        </div>
-    )
+    if (!data) {
+        return notFound()
+    } else
+        return (
+            <div className={styles.container}>
+                <CodeTitle text={data.codeTitle} />
+                <PortfolioItems data={data} />
+                <div />
+                <div />
+            </div>
+        )
 }
 
 export default Page
